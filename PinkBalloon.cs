9234d1ffa5a92f9a36b3645de2ebbbb42d5878b6
OOPGROUPSEP24 <adrianayusrii204@gmail.com>
2024-11-21 11:48:33 +0800

    Create PinkBalloon.cs

---
+++ PinkBalloon.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PinkBalloon : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (ScoreManager.Instance == null)
+        {
+            Debug.LogError("ScoreManager Instance is null!");
+            return;
+        }
+
+        // Trigger explode animation
+        Explode();
+
+        // Add points to the score
+        ScoreManager.Instance.AddScore(200);
+
+        // Destroy this balloon
+        Destroy(gameObject);
+    }
+
+    private void Explode()
+    {
+        Debug.Log("Pink Balloon Exploded!");
+    }
+}
